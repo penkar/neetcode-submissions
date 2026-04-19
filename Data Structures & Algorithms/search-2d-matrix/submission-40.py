8dd105759387d1f6e20cafd56f1e5d489abcf4e0
Jeffrey Penkar <jspenkar@gmail.com>
2026-04-19 11:09:46 -0500

    Add: search-2d-matrix - submission-40

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-40.py
@@ -0,0 +1,29 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        height = len(matrix)
+        width = len(matrix[0])
+        if height * width < 3:
+            if matrix[0][0] == target or matrix[-1][-1] == target:
+                return True
+
+        l, r = 0, height * width - 1
+        i = 0
+        while r > l:
+            i += 1
+            if i == 20:
+                break
+            middle = l + (r - l) // 2
+            currentposition = matrix[middle // width][middle % width]
+            # print('case', l, middle, r)
+            if currentposition == target:
+                return True
+            if r - l == 1:
+                currentposition2 = matrix[(middle + 1) // width][(middle + 1) % width]
+                # print('case2', currentposition2, currentposition)
+                if currentposition2 == target:
+                    return True
+            if currentposition > target:
+                r = middle
+            else:
+                l = middle
+        return False
